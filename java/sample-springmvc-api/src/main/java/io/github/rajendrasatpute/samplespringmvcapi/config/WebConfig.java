8e41970cf71bc0d19c86cdd5a513049b1ed66e34
Rajendra Satpute <rajendra.satpute@gmail.com>
2024-02-19 20:48:01 +0530

    Adds OpenApi documentation for Spring MVC

--- java/sample-springmvc-api/src/main/java/io/github/rajendrasatpute/samplespringmvcapi/config/WebConfig.java
+++ java/sample-springmvc-api/src/main/java/io/github/rajendrasatpute/samplespringmvcapi/config/WebConfig.java
@@ -1,16 +1,29 @@
 package io.github.rajendrasatpute.samplespringmvcapi.config;
 
+import org.springdoc.core.configuration.SpringDocConfiguration;
+import org.springdoc.core.properties.SwaggerUiConfigProperties;
+import org.springdoc.core.properties.SwaggerUiOAuthProperties;
+import org.springdoc.webmvc.core.configuration.SpringDocWebMvcConfiguration;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan("io.github.rajendrasatpute.samplespringmvcapi")
-public class WebConfig {
+@ComponentScan(basePackages = {"org.springdoc", "io.github.rajendrasatpute.samplespringmvcapi"})
+@Import({SpringDocConfiguration.class,
+        SpringDocWebMvcConfiguration.class,
+        org.springdoc.webmvc.ui.SwaggerConfig.class,
+        SwaggerUiConfigProperties.class,
+        SwaggerUiOAuthProperties.class,
+        JacksonAutoConfiguration.class})
+public class WebConfig implements WebMvcConfigurer {
 
     @Value("${client.openmeteo.domain}")
     private String openMeteoDomain;
